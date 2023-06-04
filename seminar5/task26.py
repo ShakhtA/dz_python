778dde704d3d242f3dc23c0f48bbb55551485077
Andrey <shakht_a@mail.ru>
2023-06-04 21:32:59 +0300

    добавлена отрицательная степень

--- seminar5/task26.py
+++ seminar5/task26.py
@@ -1,5 +1,17 @@
-a = int(input('a = '))
-b = int(input('b = '))
+
+'''
+Задача 26
+Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.
+
+Пример:
+
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 
+'''
+
+a = int(input('Введите основание степени - '))
+b = int(input('Введите показатель степени - '))
 
 def pow(a, b):
     if b == 0: return 1
@@ -8,5 +20,10 @@ def pow(a, b):
             return a
         else:
             return a * pow(a, b - 1)
+    else:
+        if b == -1:
+            return 1/a
+        else:
+            return 1/a * pow(a, b + 1)
         
-print(pow(a, b))
+print(f'A = {a}; B = {b} -> {pow(a, b)}')
